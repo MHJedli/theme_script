0971290f5c16beba32484938b47ecca91a8c4db9
MHJedli <mouadjadli10@gmail.com>
2024-09-15 16:34:27 +0100

    Add error handling and logging to Windows 7 install script

--- src/Mint/21.3/Windows-7/install_windows7.sh
+++ src/Mint/21.3/Windows-7/install_windows7.sh
@@ -4,43 +4,59 @@ path=$(pwd)/src/Mint/21.3/Windows-7
 source $(pwd)/src/utils.sh
 LOG_FILE=$(pwd)/src/logfile.log
 
-clear
-echo 'Updating Database...'
-sleep 1
-sudo apt update
+trap 'handle_error "An unexpected error occurred."' ERR
+
+echo "Continue script execution in Windows 7 Theme Installation at $(date)" >> "$LOG_FILE"
 
 clear
-echo 'Installing Dconf Editor...'
-sudo apt install dconf-editor
+if check_internet; then
+  log_message "INFO" "Internet Connection Detected. Proceeding with Theme Installation"
+
+  log_message "INFO" "Updating the DataBase"
+  echo 'Updating Database...'
+  sleep 1
+  sudo apt update || handle_error "Failed to update the Database"
+
+  clear
+  log_message "INFO" "Installing Dconf Editor"
+  echo 'Installing Dconf Editor...'
+  sudo apt install dconf-editor || handle_error "Failed to install Dconf Editor"
+
+else
+
+  handle_error "No internet Connection Available. Exiting..."
+
+fi
 
 clear
+log_message "INFO" "Extracting and Installing Windows 7 Theme"
 echo 'Extracting and Installing Theme...'
 sleep 1
-if [ ! -d $HOME/.themes ]; then
-  mkdir $HOME/.themes
-fi
-unzip $path/Windows-7-theme.zip -d $HOME/.themes
+mkdir -p $HOME/.themes || handle_error "Failed to create ~/.themes directory"
+unzip $path/Windows-7-theme.zip -d $HOME/.themes || handle_error "Failed to Extract Windows 7 Theme"
 
 clear
+log_message "INFO" "Extracting and Installing Windows 7 Icons"
 echo 'Extracting and Installing Icons...'
 sleep 1
-if [ ! -d $HOME/.icons ]; then
-  mkdir $HOME/.icons
-fi
-unzip $path/Windows-7-icons.zip -d $HOME/.icons
+mkdir -p $HOME/.icons || handle_error "Failed to Create ~/.icons directory"
+unzip $path/Windows-7-icons.zip -d $HOME/.icons || handle_error "Failed to Extract Windows 7 icons"
 
 clear
-echo "Extracting And Installing Wallpaper to $HOME/Pictures"
+log_message "INFO" "Extracting and Installing Windows 7 Wallpaper to $HOME/Pictures"
+echo "Extracting And Installing Wallpapers to $HOME/Pictures"
 sleep 1
-if [ ! -d $HOME/Pictures/Wallpapers ]; then
-  mkdir $HOME/Pictures/Wallpapers
-fi
-unzip $path/Windows-7-wallpaper.zip -d $HOME/Pictures/Wallpapers
+mkdir -p $HOME/Pictures/Wallpapers || handle_error "Failed to Create ~/Pictures/Wallpapers directory"
+unzip $path/Windows-7-wallpaper.zip -d $HOME/Pictures/Wallpapers || handle_error "Failed to Extract Windows 7 Wallpapers"
 
+log_message "INFO" "Applying Windows 7 Theme"
 echo 'Applying Windows 7 Theme'
-unzip $path/cinnamon.zip -d $HOME/.local/share/
-dconf load / < $path/windows-7-config
-gsettings set org.cinnamon.desktop.background picture-uri file://$HOME/Pictures/Wallpapers/windows-7-wallpaper.jpg
-clear
+unzip $path/cinnamon.zip -d $HOME/.local/share/ || handle_error "Failed to Extract Cinnamon Extensions"
+dconf load / < $path/windows-7-config || handle_error "Failed to load Windows 7 Configs"
+gsettings set org.cinnamon.desktop.background picture-uri file://$HOME/Pictures/Wallpapers/windows-7-wallpaper.jpg || handle_error "Failed to apply Windows 7 Wallpaper"
 
+clear
+log_message "INFO" "Siging Out"
 signOut "cinnamon-session-quit --logout --force"
+
+log_message "INFO" "Windows 7 Installation Script ended succesfully"
