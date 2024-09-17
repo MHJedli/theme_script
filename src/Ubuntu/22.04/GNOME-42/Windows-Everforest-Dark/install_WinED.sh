64647ff9d4c3501034b70db249f97133f8964ef9
MHJedli <mouadjadli10@gmail.com>
2024-09-17 11:39:46 +0100

    Add Logging and Error Handling to WIndows Everforest Dark Theme Script

--- src/Ubuntu/22.04/GNOME-42/Windows-Everforest-Dark/install_WinED.sh
+++ src/Ubuntu/22.04/GNOME-42/Windows-Everforest-Dark/install_WinED.sh
@@ -4,36 +4,59 @@ path=$(pwd)/src/Ubuntu/22.04/GNOME-42/Windows-Everforest-Dark
 source $(pwd)/src/utils.sh
 LOG_FILE=$(pwd)/src/logfile.log
 
-echo "Updating the database..."
-sleep 1
-sudo apt update
+trap 'handle_error "An unexpected error occurred."' ERR
+
 clear
 
-echo "Installing Dconf Editor , GNOME Tweaks and GNOME Extensions..."
+echo "Continue script execution in Windows Everforest Dark Theme Installation at $(date)" >> "$LOG_FILE"
+
+if check_internet; then
+
+    log_message "INFO" "Internet Connection Detected. Proceeding with Theme Installation"
+
+    log_message "INFO" "Updating the database"
+    echo "Updating the database..."
+    sleep 1
+    sudo apt update || handle_error "Failed to update the database"
+    clear
+
+    log_message "INFO" "Installing Dconf Editor , GNOME Tweaks and GNOME Extensions"
+    echo "Installing Dconf Editor , GNOME Tweaks and GNOME Extensions..."
+    sleep 1
+    sudo apt install dconf-editor gnome-tweaks gnome-shell-extensions -y || handle_error "Failed to install packages"
+    clear
+
+else
+
+    handle_error "No internet Connection Available. Exiting..."
+
+fi
+
+log_message "INFO" "Installing Themes and Icons"
+echo "Installing Themes, Icons and Fonts..."
 sleep 1
-sudo apt install dconf-editor gnome-tweaks gnome-shell-extensions
+tar --extract --file $path/my-icons.tar.gz -C ~/ --strip-components=2 || handle_error "Failed to extract Icons"
+tar --extract --file $path/my-themes.tar.gz -C ~/ --strip-components=2 || handle_error "Failed to extract Themes"
 clear
 
-echo "Installing Themes and Icons..."
+log_message "INFO" "Installing The Extensions"
+echo "Installing The Extensions..."
 sleep 1
-tar --extract --file $path/my-icons.tar.gz -C ~/ --strip-components=2
-tar --extract --file $path/my-themes.tar.gz -C ~/ --strip-components=2
+tar --extract --file $path/my-extensions.tar.gz -C ~/ --strip-components=2 || handle_error "Failed to extract the Extensions"
 clear
 
-echo "Installing the extensions..."
+log_message "INFO" "Windows Everforest Dark Theme Configurations"
+echo "Windows Everforest Dark Theme Configurations..."
 sleep 1
-tar --extract --file $path/my-extensions.tar.gz -C ~/ --strip-components=2
+dconf load / < $path/full-gnome-settings || handle_error "Failed to apply Windows Everforest Dark Theme Configurations"
 clear
 
-echo "Applying The Custom DE Settings..."
-sleep 1
-dconf load / < $path/full-gnome-settings
-clear
-
+log_message "INFO" "Extracting Wallpapers in ~/Pictures and Applying Background"
 echo "Extracting Wallpapers in ~/Pictures and Applying Background..."
 sleep 1
-cp $path/Background/wallpaper.jpg ~/Pictures
-gsettings set org.gnome.desktop.background picture-uri-dark file://$HOME/Pictures/wallpaper.jpg
+cp $path/Background/wallpaper.jpg ~/Pictures || handle_error "Failed to copy Wallpapers to ~/Pictures"
+gsettings set org.gnome.desktop.background picture-uri-dark file://$HOME/Pictures/wallpaper.jpg || handle_error "Failed to Apply Background Wallpaper"
 clear
 
 signOut "gnome-session-quit --no-prompt"
+log_message "INFO" "Windows Everforest Dark Theme Installation Script Ended Succesfully"
